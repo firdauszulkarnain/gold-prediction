d0e9521be639f6f27d52f4e76d2cbb5dd560ef6d
firdauszulkarnain <muhammadfirdauszulkarnain03@gmail.com>
2022-06-29 19:31:39 +0800

    init

---
+++ application/views/dashboard.php
@@ -0,0 +1,15 @@
+<section class="content-header">
+    <div class="container-fluid">
+        <div class="row mb-2">
+
+        </div>
+    </div>
+</section>
+
+<section class="content mt-5">
+    <div class="row d-fle justify-content-center">
+        <div class="col-lg-10">
+            <h1 class="text-center">Selamat Datang di Sistem Prediksi <br> Harga Emas</h1>
+        </div>
+    </div>
+</section>
---
+++ application/views/template/template.php
@@ -0,0 +1,171 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>AdminLTE 3 | Blank Page</title>
+
+    <!-- Font Awesome -->
+    <link rel="stylesheet" href="<?= base_url() ?>assets/plugins/fontawesome-free/css/all.min.css">
+    <!-- Theme style -->
+    <link rel="stylesheet" href="<?= base_url() ?>assets/css/adminlte.min.css">
+    <!-- DATATABLE -->
+    <link rel="stylesheet" href="<?= base_url() ?>assets/plugins/datatables-bs4/css/dataTables.bootstrap4.min.css">
+    <link rel="stylesheet" href="<?= base_url() ?>assets/plugins/datatables-responsive/css/responsive.bootstrap4.min.css">
+    <!-- SELECTPICKER -->
+    <link rel="stylesheet" href="<?= base_url() ?>assets/css/bootstrap-select/bootstrap-select.css">
+    <!-- DATEPICKER -->
+    <link rel="stylesheet" href="<?= base_url() ?>assets/css/bootstrap-datepicker/bootstrap-datepicker.css">
+    <!-- My Style -->
+    <link rel="stylesheet" href="<?= base_url() ?>assets/css/myadmin.css">
+
+    <!-- jQuery -->
+    <script src="<?= base_url() ?>assets/plugins/jquery/jquery.min.js"></script>
+    <!-- DATEPICKER -->
+    <script src="<?= base_url() ?>assets/js/bootstrap-datepicker/bootstrap-datepicker.js"></script>
+</head>
+
+<body class="hold-transition sidebar-mini">
+    <!-- Site wrapper -->
+    <div class="wrapper">
+        <!-- Navbar -->
+        <nav class="main-header navbar navbar-expand navbar-white navbar-light">
+            <!-- Left navbar links -->
+            <ul class="navbar-nav">
+                <li class="nav-item">
+                    <a class="nav-link" data-widget="pushmenu" href="#" role="button"><i class="fas fa-bars"></i></a>
+                </li>
+                <li class="nav-item d-none d-sm-inline-block">
+                    <a href="../../index3.html" class="nav-link">Prediksi Harga Emas</a>
+                </li>
+            </ul>
+
+            <!-- Right navbar links -->
+            <ul class="navbar-nav ml-auto">
+                <li class="nav-item">
+                    <a class="nav-link" data-widget="fullscreen" href="#" role="button">
+                        <i class="fas fa-expand-arrows-alt"></i>
+                    </a>
+                </li>
+            </ul>
+        </nav>
+        <!-- /.navbar -->
+
+        <!-- Main Sidebar Container -->
+        <aside class="main-sidebar sidebar-dark-primary elevation-4">
+            <!-- Brand Logo -->
+            <a href="../../index3.html" class="brand-link">
+                <img src="<?= base_url() ?>assets/img/logo/AdminLTELogo.png" alt="AdminLTE Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
+                <span class="brand-text font-weight-light">AdminLTE 3</span>
+            </a>
+
+            <!-- Sidebar -->
+            <div class="sidebar">
+
+                <!-- Sidebar Menu -->
+                <nav class="mt-2">
+                    <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
+                        <li class="nav-item mt-2">
+                            <a href="<?= base_url() ?>dashboard" class="nav-link <?= $this->uri->segment(1) == 'dashboard'  ? "active" : "" ?>">
+                                <i class="nav-icon fas fa-tachometer-alt"></i>
+                                <p>
+                                    Dashboard
+                                </p>
+                            </a>
+                        </li>
+                        <li class="nav-item mt-2">
+                            <a href="<?= base_url() ?>prediction" class="nav-link <?= $this->uri->segment(1) == 'prediction'  ? "active" : "" ?>">
+                                <i class="nav-icon fas fa-wave-square"></i>
+                                <p>
+                                    Prediction
+                                </p>
+                            </a>
+                        </li>
+                        <li class="nav-item mt-2">
+                            <a href="<?= base_url() ?>harga/emas" class="nav-link <?= $this->uri->segment(1) == 'harga'  ? "active" : "" ?>">
+                                <i class="nav-icon fas fa-stop-circle"></i>
+                                <p>
+                                    Data Harga Emas
+                                </p>
+                            </a>
+                        </li>
+                        <li class="nav-item mt-2">
+                            <a href="<?= base_url() ?>about" class="nav-link <?= $this->uri->segment(1) == 'about'  ? "active" : "" ?>">
+                                <i class="nav-icon fas fa-question-circle"></i>
+                                <p>
+                                    About
+                                </p>
+                            </a>
+                        </li>
+                    </ul>
+                </nav>
+                <!-- /.sidebar-menu -->
+            </div>
+            <!-- /.sidebar -->
+        </aside>
+
+        <!-- MAIN CONTENT -->
+        <div class="content-wrapper">
+            <div class="flash-data" data-flashdata="<?= $this->session->flashdata('pesan'); ?>"></div>
+            <?= $contens ?>
+        </div>
+        <!-- END MAINCONTENT -->
+
+
+        <footer class="main-footer">
+            <strong>Copyright &copy; <?= date('Y') ?> Sistem Prediksi Emas All rights reserved.
+        </footer>
+
+        <!-- Control Sidebar -->
+        <aside class="control-sidebar control-sidebar-dark">
+            <!-- Control sidebar content goes here -->
+        </aside>
+        <!-- /.control-sidebar -->
+    </div>
+    <!-- ./wrapper -->
+
+
+    <!-- Logout Modal-->
+    <div class="modal fade" id="logoutModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+        <div class="modal-dialog modal-lg" role="document">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title" id="exampleModalLabel">Logout</h5>
+                    <button class="close" type="button" data-dismiss="modal" aria-label="Close">
+                        <span aria-hidden="true">×</span>
+                    </button>
+                </div>
+                <div class="modal-body">Apakah anda yakin untuk keluar dari aplikasi?</div>
+                <div class="modal-footer">
+                    <button class="btn btn-secondary" type="button" data-dismiss="modal">Cancel</button>
+                    <a class="btn btn-danger" href="<?= base_url() ?>auth/logout">Logout</a>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <!-- Bootstrap 4 -->
+    <script src="<?= base_url() ?>assets/plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
+    <!-- DATA TABLE -->
+    <script src="<?= base_url() ?>assets/plugins/datatables/jquery.dataTables.min.js"></script>
+    <script src="<?= base_url() ?>assets/plugins/datatables-bs4/js/dataTables.bootstrap4.min.js"></script>
+    <script src="<?= base_url() ?>assets/plugins/datatables-responsive/js/dataTables.responsive.min.js"></script>
+    <script src="<?= base_url() ?>assets/plugins/datatables-responsive/js/responsive.bootstrap4.min.js"></script>
+    <!-- AdminLTE App -->
+    <script src="<?= base_url() ?>assets/js/adminlte.min.js"></script>
+    <!-- SWEETALERT2 -->
+    <script src="<?= base_url() ?>assets/plugins/sweetalert2/sweetalert2.all.min.js"></script>
+    <!-- SELECT PICKER -->
+    <script src="<?= base_url() ?>assets/js/bootstrap-select/bootstrap-select.js"></script>
+    <script src="<?= base_url('assets/js/select/defaults-id_ID.min.js') ?>"></script>
+    <!-- JQUERY MASKING Money -->
+    <script src="<?= base_url() ?>assets/js/jquery-mask/jquery.mask.js"></script>
+
+    <!-- FileStyle -->
+    <script type="text/javascript" src="<?= base_url() ?>assets/js/bootstrap-filestyle/bootstrap-filestyle.min.js"> </script>
+    <!-- MYJS -->
+    <script src="<?= base_url() ?>assets/js/myjs.js"></script>
+</body>
+
+</html>
