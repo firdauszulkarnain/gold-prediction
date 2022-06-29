ac7f22b4e09afe0dbbff74ff48a561d2582438b3
firdauszulkarnain <muhammadfirdauszulkarnain03@gmail.com>
2022-06-29 22:27:46 +0800

    update

---
+++ application/controllers/Admin.php
@@ -0,0 +1,74 @@
+<?php
+defined('BASEPATH') or exit('No direct script access allowed');
+
+class Admin extends CI_Controller
+{
+    // Cek Session Login Admin
+    public function __construct()
+    {
+        parent::__construct();
+        if (!$this->session->userdata('admin')) {
+            redirect('auth/login');
+        }
+    }
+
+    public function index()
+    {
+        redirect('admin/dashboard');
+    }
+
+    public function dashboard()
+    {
+        $data['admin'] = $this->db->get_where('admin', ['username' => $this->session->userdata('admin')])->row_array();
+        $data['title'] = 'Dashboard';
+        $this->template->load('template/template', 'admin/dashboard', $data);
+    }
+
+    public function profile()
+    {
+        $data['admin'] = $this->db->get_where('admin', ['username' => $this->session->userdata('admin')])->row_array();
+        $data['title'] = 'Profil Admin';
+        $this->template->load('template/template', 'admin/profile/index', $data);
+    }
+
+    public function ganti_password()
+    {
+        $data['admin'] = $this->db->get_where('admin', ['username' => $this->session->userdata('admin')])->row_array();
+        $admin_id = $data['admin']['id_admin'];
+        $admin_password = $data['admin']['password'];
+        $this->form_validation->set_rules(
+            'old_password',
+            'Password',
+            'required|trim',
+            [
+                'required' => 'Password Lama Tidak Boleh Kosong',
+            ]
+        );
+
+        $this->form_validation->set_rules(
+            'password1',
+            'Password',
+            'required|trim|min_length[6]|matches[password2]',
+            [
+                'required' => 'Password Baru Tidak Boleh Kosong',
+                'matches' => 'Konfirmasi Password Tidak Sesuai',
+                'min_length' => 'Minimal Password 6 Karakter'
+            ]
+        );
+
+        $this->form_validation->set_rules('password2', 'Password', 'required|trim');
+        $password = $this->input->post('old_password');
+        if ($this->form_validation->run() == false) {
+            redirect('admin/profile');
+        } else {
+            if (password_verify($password, $admin_password)) {
+                $this->Auth_Model->update_password($admin_id);
+                $this->session->set_flashdata('pesan', 'Update Password');
+                redirect('admin/profile');
+            } else {
+                $this->session->set_flashdata('old_password', 'Password Lama Tidak Sesuai');
+                redirect('admin/profile');
+            }
+        }
+    }
+}
---
+++ application/views/auth/login.php
@@ -0,0 +1,99 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title><?= $title; ?></title>
+
+    <!-- Google Font: Source Montserrat -->
+    <link href="https://fonts.googleapis.com/css2?family=Montserrat&display=swap" rel="stylesheet">
+    <!-- Font Awesome -->
+    <link rel="stylesheet" href="<?= base_url() ?>assets/plugins/fontawesome-free/css/all.min.css">
+    <!-- Theme style -->
+    <link rel="stylesheet" href="<?= base_url() ?>assets/css/adminlte.min.css">
+    <!-- My Style -->
+    <link rel="stylesheet" href="<?= base_url() ?>assets/css/auth.css">
+</head>
+
+<body class="bg-warning">
+    <div class="container">
+        <div class="row mt-5 d-flex justify-content-center">
+            <div class="login-box mt-5" style="opacity: 0.95 !important;">
+                <div class="flash-data" data-flashdata="<?= $this->session->flashdata('pesan'); ?>"></div>
+                <div class="card">
+                    <div class="pt-5 pb-3 px-4">
+                        <div class=" card-body p-0">
+                            <div class="text-center">
+                                <h1 class="h4 font-weight-bolder mt-1" style="color: black !important;"> LOGIN SISTEM</h1>
+                                <hr class="mb-4 garis">
+                            </div>
+                            <div class="error" data-error="<?= $this->session->flashdata('error'); ?>"></div>
+                            <form method="POST" action="<?= base_url('auth/login') ?>">
+                                <div class="mb-3">
+                                    <div class="input-group <?= (form_error('username')) ? 'is-invalid' : '' ?>">
+                                        <div class="input-group-prepend">
+                                            <span class="input-group-text bg-light"> <span class="fas fa-fw fa-user"></span></span>
+                                        </div>
+                                        <input type="text" class="form-control <?= (form_error('username')) ? 'is-invalid' : '' ?>" placeholder="Username" value="<?= set_value('username') ?>" name="username" autocomplete="off" autofocus>
+                                    </div>
+                                    <div class="invalid-feedback">
+                                        <?= form_error('username') ?>
+                                    </div>
+                                </div>
+
+
+                                <div class="mb-4">
+                                    <div class="input-group <?= (form_error('password')) ? 'is-invalid' : '' ?>">
+                                        <div class="input-group-prepend">
+                                            <span class="input-group-text bg-light"> <span class="fas fa-fw fa-lock"></span></span>
+                                        </div>
+                                        <input type="password" class="form-control <?= (form_error('password')) ? 'is-invalid' : '' ?>" placeholder="Password" name="password" autocomplete="off">
+
+                                    </div>
+                                    <div class="invalid-feedback">
+                                        <?= form_error('password') ?>
+                                    </div>
+                                </div>
+                                <button type="submit" class="btn btn-warning btn-block mt-4 font-weight-bolder mb-5">Masuk</button>
+                            </form>
+                        </div>
+
+                    </div>
+                </div>
+
+            </div>
+        </div>
+    </div>
+
+
+
+    <script src="<?= base_url() ?>assets/plugins/jquery/jquery.min.js"></script>
+    <script src="<?= base_url() ?>assets/plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
+    <script src="<?= base_url() ?>assets/js/adminlte.min.js"></script>
+    <!-- SWEETALERT2 -->
+    <script src="<?= base_url() ?>assets/plugins/sweetalert2/sweetalert2.all.min.js"></script>
+    <script>
+        $(document).ready(function() {
+
+            const flashData = $('.flash-data').data('flashdata');
+            if (flashData) {
+                Swal.fire({
+                    title: 'Success',
+                    text: flashData,
+                    icon: 'success'
+                });
+            }
+            const error = $('.error').data('error');
+            if (error) {
+                Swal.fire({
+                    title: 'Error!',
+                    text: error,
+                    icon: 'error'
+                });
+            }
+        });
+    </script>
+</body>
+
+</html>
